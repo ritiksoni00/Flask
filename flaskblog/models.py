afc29b15116cbbf9f5dc77265826ff6713e3fc34
ritiksoni00 <rtksoni00@gmail.com>
2020-10-19 23:33:31 +0530

    just want to finish it

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from flaskblog import db, login_manager
 from flask_login import UserMixin
--- run.py
+++ run.py
@@ -2,4 +2,4 @@
 from flaskblog import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8086)
+    app.run(debug=True, port=8014)
